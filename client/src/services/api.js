62118284e30d27c54a59cc97504c0ffd357b3e77
RaghavendraDerangula <raghavendraderangulaa@gmail.com>
2026-05-22 13:57:11 +0530

    Fixed auth API route

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'https://zepnest-backend.onrender.com'
+  baseURL: 'https://zepnest-backend.onrender.com/api/auth'
 });
 
 API.interceptors.request.use((req) => {
